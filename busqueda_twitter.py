df159e9a8ec516a71ad2a823e79e0e7170806bdf
María Eugenia Escobar <kena.escobarm@gmail.com>
2021-02-14 21:50:34 -0300

    versión logra crear csv falta agregar tiempos para que no aborte petición a la API

--- busqueda_twitter.py
+++ busqueda_twitter.py
@@ -3,10 +3,17 @@
 # Importamos la librería
 import tweepy
 import pandas as pd
-from textblob import Texblob
+from textblob import TextBlob
 import json
 import numpy as np 
-import nltk
+import matplotlib.pyplot as plt
+import re
+#import nlkt
+#nlkt.downloads ('stopwords')
+#from nltk.corpus import stopwords
+#from nlkt.stem.porter import PorterStemmer
+#from wordcloud import WordCloud
+#from collections import Counter
 
 # Pasamos los tokens de cliente
 consumer_key = "SFPCBPyTxra10ftDpA9ekrPOW"
@@ -25,8 +32,8 @@ api = tweepy.API(auth)
 #Busqueda usando search
 api = tweepy.API(auth)
 
-clave_busqueda = "mapuche"
-cantidad = 10
+clave_busqueda = "mapuche constituyente"
+cantidad = 1000
 
 search_twetter = [status for status in tweepy.Cursor(api.search, q = clave_busqueda).items(cantidad)]
 
@@ -41,12 +48,62 @@ f = open('twitter.txt', 'a', encoding = 'utf-8')
 f.write(str(resultado))
 f.close()
 """
-
-#Analisis de datos obtenidos 
+#Analisis de datos obtenidos NLKT
 positivos = 0
 negativos = 0
 neutros = 0
 
-for tweet in search_twetter:
-    analysis = Texblob(tweet.text)
-    if analysis.sen
+"""for tweet in search_twetter:
+    analysis = Textblob(tweet.text)
+    if analysis.sentiment [0] > 0:
+        positivos = positivos +1
+    elif analysis.sentiment [0] > 0:
+        negativos = negativos +1
+    else:
+        neutros = neutros +1
+
+print('Total Positivos ' , positivos)
+print('Total Negativos ' , negativos)
+print('Total Neutrales ' , neutros)
+
+#Plotting sentiments
+labels = 'Positive', 'Negative', 'Neutral'
+sizes = [257, 223, 520]
+colors = ['gold', 'yellowgreen', 'lightcoral']
+explode = (0.1, 0, 0)  # explode 1ª slice
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()"""
+
+#Creación de DataFrame
+my_list_of_dicts = []
+for each_json in search_twetter:
+    my_list_of_dicts.append(each_json._json)
+
+with open('twetter_json_Data.txt', 'w' ) as file :
+    file.write(json.dumps(my_list_of_dicts,indent=4))
+
+my_demo_list = []
+with open('twetter_json_Data.txt', encoding= 'utf-8') as json_file:
+    all_data = json.load (json_file)
+    for each_diccionary in all_data:
+
+        tweet_id = each_diccionary ['id']
+        text = each_diccionary['text']
+        favorite_count = each_diccionary ['favorite_count']
+        retweet_count = each_diccionary ['retweet_count']
+        created_at = each_diccionary ['created_at']
+
+        my_demo_list.append ({'tweet_id': str (tweet_id),'text':str (text),
+         'favorite_count': int (favorite_count), 'retweet_count': int (retweet_count),
+         'created_at': created_at,})
+
+        twette_dataset = pd.DataFrame (my_demo_list, columns = ['tweet_id' , 'text',
+        'favorite_count', 'retwett_count' , 'create_count'])
+        # Escribir para futuras referencias a scv 
+        twette_dataset.to_csv('twetter_data.csv')        
+
+
+
+
+
