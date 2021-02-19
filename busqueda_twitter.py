e8058ab2a3bc7579aac5ca1a8634b30e8d892fd6
María Eugenia Escobar <kena.escobarm@gmail.com>
2021-02-19 14:29:44 -0300

    Soluciona información del usuario, comenta filtros repetidos porqué RT solo se filtran en local!

--- busqueda_twitter.py
+++ busqueda_twitter.py
@@ -22,7 +22,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit= True ,wait_on_rate_limit_notify= True)
 
 clave_busqueda = "mapuche constituyente"
-cantidad = 500
+cantidad = 600
 search_twetter = [status for status in tweepy.Cursor(api.search, q = "mapuche", tweet_mode='extended').items(cantidad)]
 
 
@@ -30,8 +30,8 @@ search_twetter = [status for status in tweepy.Cursor(api.search, q = "mapuche", 
 
 my_list_of_dicts = []
 for each_json in search_twetter:
-    if each_json.full_text.startswith('RT'):
-        continue
+    #if each_json.full_text.startswith('RT'): #El if elimina los duplicados pero local, el mayor % de datos son RT
+        #continue
     my_list_of_dicts.append(each_json._json)
 
 with open('twetter_json_Data.txt', 'w' ) as file :
@@ -42,35 +42,34 @@ with open('twetter_json_Data.txt', encoding= 'utf-8') as json_file:
     all_data = json.load (json_file)
     for each_diccionary in all_data:
     
-        #print(each_diccionary)
+        print(each_diccionary['user']['name'])
 
         created_at = each_diccionary ['created_at']
         tweet_id = each_diccionary ['id']
         full_text = each_diccionary['full_text']
 
     #formila trae todo lo que está en user. No filtra el dato especifico que se requiere.
-        screen_name = each_diccionary['user'],['screen_name']
-        name = each_diccionary ['user'],['name']
-        location = each_diccionary ['user'],['location']
+        screen_name = each_diccionary['user']['screen_name']
+        name = each_diccionary ['user']['name']
+        location = each_diccionary ['user']['location']
 
-        followers_count = each_diccionary ['user'],['followers_count']
-        friends_count = each_diccionary ['user'],['friends_count']
-        retweet_count = each_diccionary ['user'],['retweet_count']
+        followers_count = each_diccionary ['user']['followers_count']
+        friends_count = each_diccionary ['user']['friends_count']
+        
       
 
         my_demo_list.append ({
+            'created_at': str (created_at),
             'tweet_id':str (tweet_id),
         'full_text':str (full_text),
         'screen_name':str (screen_name),
         'name':str (name),
         'location':str (location),
         'followers_count': str (followers_count), 
-        'friends_count': str (friends_count), 
-        'retweet_count': str (retweet_count),
-        'created_at': created_at,})
+        'friends_count': str (friends_count),})
 
-        twette_dataset = pd.DataFrame (my_demo_list, columns = ['tweet_id' , 'full_text','screen_name',
-        'name','location','followers_count','friends_count', 'retwett_count' , 'create_count'])
+        twette_dataset = pd.DataFrame (my_demo_list, columns = ['created_at','tweet_id' , 'full_text','screen_name',
+        'name','location','followers_count','friends_count',])
         # Escribir para futuras referencias a scv 
         twette_dataset.to_csv('twetter_data.csv')      
 
